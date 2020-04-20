2f10dac6b545f94dc8bd866affa7286151507e0a
Hamza Waseem <hamzawaseem_7@live.com>
2020-04-20 15:28:55 -0400

    Added custom filter

--- colorblinding.js
+++ colorblinding.js
@@ -1,6 +1,5 @@
-function appendSVG(document) {
-
-    /*
+function appendSVG(document, customValues) {
+  /*
      'Normal':[1,0,0,0,0, 0,1,0,0,0, 0,0,1,0,0, 0,0,0,1,0, 0,0,0,0,1],
      'Protanopia':[0.567,0.433,0,0,0, 0.558,0.442,0,0,0, 0,0.242,0.758,0,0, 0,0,0,1,0, 0,0,0,0,1],
      'Protanomaly':[0.817,0.183,0,0,0, 0.333,0.667,0,0,0, 0,0.125,0.875,0,0, 0,0,0,1,0, 0,0,0,0,1],
@@ -9,59 +8,147 @@ function appendSVG(document) {
      'Tritanopia':[0.95,0.05,0,0,0, 0,0.433,0.567,0,0, 0,0.475,0.525,0,0, 0,0,0,1,0, 0,0,0,0,1],
      'Tritanomaly':[0.967,0.033,0,0,0, 0,0.733,0.267,0,0, 0,0.183,0.817,0,0, 0,0,0,1,0, 0,0,0,0,1],
      'Achromatopsia':[0.299,0.587,0.114,0,0, 0.299,0.587,0.114,0,0, 0.299,0.587,0.114,0,0, 0,0,0,1,0, 0,0,0,0,1],
-     'Achromatomaly':[0.618,0.320,0.062,0,0, 0.163,0.775,0.062,0,0, 0.163,0.320,0.516,0,0,0,0,0,1,0,0,0,0,0]
+     'Achromatomaly':[0.618,0.320,0.062,0,0, 0.163,0.775,0.062,0,0, 0.163,0.320,0.516,0,0, 0,0,0,1,0 ,0,0,0,0,1]
      */
 
-    var svg = '<svg id="colorBlindSVG" version="1.1" xmlns="http://www.w3.org/2000/svg" baseProfile="full"> <filter id="protanopia"> <feColorMatrix type="matrix" values="0.567, 0.433, 0, 0, 0, 0.558, 0.442, 0, 0, 0, 0, 0.242, 0.758, 0, 0, 0, 0, 0, 1, 0" in="SourceGraphic" /> </filter> <filter id="protanomaly"> <feColorMatrix type="matrix" values="0.817,0.183,0,0,0 0.333,0.667,0,0,0 0,0.125,0.875,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> <filter id="deuteranopia"> <feColorMatrix type="matrix" values="0.625,0.375,0,0,0 0.7,0.3,0,0,0 0,0.3,0.7,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> <filter id="deuteranomaly"> <feColorMatrix type="matrix" values="0.8,0.2,0,0,0 0.258,0.742,0,0,0 0,0.142,0.858,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> <filter id="tritanopia"> <feColorMatrix type="matrix" values="0.95,0.05,0,0,0 0,0.433,0.567,0,0 0,0.475,0.525,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> <filter id="tritanomaly"> <feColorMatrix type="matrix" values="0.967,0.033,0,0,0 0,0.733,0.267,0,0 0,0.183,0.817,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> <filter id="achromatopsia"> <feColorMatrix type="matrix" values="0.299,0.587,0.114,0,0 0.299,0.587,0.114,0,0 0.299,0.587,0.114,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> <filter id="achromatomaly"> <feColorMatrix type="matrix" values="0.618,0.320,0.062,0,0 0.163,0.775,0.062,0,0 0.163,0.320,0.516,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> </svg>';
+  // 567
+  var filterProtanopia =
+    '<filter id="protanopia"><feColorMatrix type="matrix" values="0.567, 0.433, 0, 0, 0, 0.558, 0.442, 0, 0, 0, 0, 0.242, 0.758, 0, 0, 0, 0, 0, 1, 0" in="SourceGraphic" /> </filter>';
+  var filterProtanomaly =
+    '<filter id="protanomaly"> <feColorMatrix type="matrix" values="0.817,0.183,0,0,0 0.333,0.667,0,0,0 0,0.125,0.875,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> ';
+  var filterDeuteranopia =
+    '<filter id="deuteranopia"> <feColorMatrix type="matrix" values="0.625,0.375,0,0,0 0.7,0.3,0,0,0 0,0.3,0.7,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> ';
+  var filterDeuteranomaly =
+    '<filter id="deuteranomaly"> <feColorMatrix type="matrix" values="0.8,0.2,0,0,0 0.258,0.742,0,0,0 0,0.142,0.858,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> ';
+  var filterTritanopia =
+    '<filter id="tritanopia"> <feColorMatrix type="matrix" values="0.95,0.05,0,0,0 0,0.433,0.567,0,0 0,0.475,0.525,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> ';
+  var filterTritanomaly =
+    '<filter id="tritanomaly"> <feColorMatrix type="matrix" values="0.967,0.033,0,0,0 0,0.733,0.267,0,0 0,0.183,0.817,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter>';
+  var filterAchromatopsia =
+    '<filter id="achromatopsia"> <feColorMatrix type="matrix" values="0.299,0.587,0.114,0,0 0.299,0.587,0.114,0,0 0.299,0.587,0.114,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter>';
+  var filterAchromatomaly =
+    '<filter id="achromatomaly"> <feColorMatrix type="matrix" values="0.618,0.320,0.062,0,0 0.163,0.775,0.062,0,0 0.163,0.320,0.516,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter>';
 
-    var blockColorblindContent = document.getElementById("blockColorblindContent");
-    if (blockColorblindContent !== undefined && blockColorblindContent !== null) {
-        blockColorblindContent.parentNode.removeChild(blockColorblindContent);
+  //debugger;
+  var filterCustom =
+    '<filter id="custom"> <feColorMatrix type="matrix" values=';
+  console.log(customValues);
+  if (customValues !== {}) {
+    if (!String.prototype.format) {
+      String.prototype.format = function () {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function (match, number) {
+          return typeof args[number] != "undefined" ? args[number] : match;
+        });
+      };
     }
 
-    var iDiv = document.createElement('div');
-    iDiv.id = 'blockColorblindContent';
-    iDiv.innerHTML = svg;
-    iDiv.style.display = 'none'; //Fix issue "blockColorblindContent" is showing and taking up space on some websites #5
-    document.getElementsByTagName('body')[0].appendChild(iDiv);
+    var values = '"{0},{1},{2},{3},0 {4},{5},{6},{7},0 {8},{9},{10},{11},0 0,0,0,1,0"'.format(
+      customValues.r1,
+      customValues.r2,
+      customValues.r3,
+      customValues.r4,
+      customValues.g1,
+      customValues.g2,
+      customValues.g3,
+      customValues.g4,
+      customValues.b1,
+      customValues.b2,
+      customValues.b3,
+      customValues.b4
+    );
+    filterCustom += values;
+  } else {
+    filterCustom += '"1,0,0,0,0 0,1,0,0,0 0,0,1,0,0 0,0,0,1,0"';
+  }
+  filterCustom += ' in="SourceGraphic" /> </filter>';
+  console.log(filterCustom);
+
+  var svgA =
+    '<svg id="colorBlindSVG" version="1.1" xmlns="http://www.w3.org/2000/svg" baseProfile="full">';
+  svgA += filterProtanopia;
+  svgA += filterProtanomaly;
+  svgA += filterDeuteranopia;
+  svgA += filterDeuteranomaly;
+  svgA += filterTritanopia;
+  svgA += filterTritanomaly;
+  svgA += filterAchromatopsia;
+  svgA += filterAchromatomaly;
+  svgA += filterCustom;
+  svgA += "</svg>";
+
+  // var svg = '<svg id="colorBlindSVG" version="1.1" xmlns="http://www.w3.org/2000/svg" baseProfile="full"> <filter id="protanopia"> <feColorMatrix type="matrix" values="0.567, 0.433, 0, 0, 0, 0.558, 0.442, 0, 0, 0, 0, 0.242, 0.758, 0, 0, 0, 0, 0, 1, 0" in="SourceGraphic" /> </filter> <filter id="protanomaly"> <feColorMatrix type="matrix" values="0.817,0.183,0,0,0 0.333,0.667,0,0,0 0,0.125,0.875,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> <filter id="deuteranopia"> <feColorMatrix type="matrix" values="0.625,0.375,0,0,0 0.7,0.3,0,0,0 0,0.3,0.7,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> <filter id="deuteranomaly"> <feColorMatrix type="matrix" values="0.8,0.2,0,0,0 0.258,0.742,0,0,0 0,0.142,0.858,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> <filter id="tritanopia"> <feColorMatrix type="matrix" values="0.95,0.05,0,0,0 0,0.433,0.567,0,0 0,0.475,0.525,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> <filter id="tritanomaly"> <feColorMatrix type="matrix" values="0.967,0.033,0,0,0 0,0.733,0.267,0,0 0,0.183,0.817,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> <filter id="achromatopsia"> <feColorMatrix type="matrix" values="0.299,0.587,0.114,0,0 0.299,0.587,0.114,0,0 0.299,0.587,0.114,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> <filter id="achromatomaly"> <feColorMatrix type="matrix" values="0.618,0.320,0.062,0,0 0.163,0.775,0.062,0,0 0.163,0.320,0.516,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> </svg>';
+
+  var blockColorblindContent = document.getElementById(
+    "blockColorblindContent"
+  );
+  if (blockColorblindContent !== undefined && blockColorblindContent !== null) {
+    blockColorblindContent.parentNode.removeChild(blockColorblindContent);
+  }
+
+  var iDiv = document.createElement("div");
+  iDiv.id = "blockColorblindContent";
+  iDiv.innerHTML = svgA;
+  iDiv.style.display = "none"; //Fix issue "blockColorblindContent" is showing and taking up space on some websites #5
+  document.getElementsByTagName("body")[0].appendChild(iDiv);
+
+  //console.log(customValues);
 }
 
-function changeColors(type) {
-    appendSVG(document);
-    revertColors(document);
+function changeColors(type, customValues) {
+  console.log("Changing colors...");
+  appendSVG(document, customValues);
+  revertColors(document);
 
-    var css = 'html {' +
-        'filter: url(#' + type + '); -webkit-filter: url(#' + type + '); -moz-filter: url(#' + type + '); -o-filter: url(#' + type + '); -ms-filter: url(#' + type + '); ' +
-        '}';
+  var css =
+    "html {" +
+    "filter: url(#" +
+    type +
+    "); -webkit-filter: url(#" +
+    type +
+    "); -moz-filter: url(#" +
+    type +
+    "); -o-filter: url(#" +
+    type +
+    "); -ms-filter: url(#" +
+    type +
+    "); " +
+    "}";
 
-    applyingStyle(document, css);
+  applyingStyle(document, css);
 }
 
 function revertColors(document) {
-    var css = 'html { -webkit-filter: none; -moz-filter: none; -o-filter: none; -ms-filter: none; } #blockColorblindContent { display: none; }';
-    applyingStyle(document, css);
+  var css =
+    "html { -webkit-filter: none; -moz-filter: none; -o-filter: none; -ms-filter: none; } #blockColorblindContent { display: none; }";
+  applyingStyle(document, css);
 }
 
 function applyingStyle(document, css) {
+  var head = document.getElementsByTagName("head")[0],
+    style = document.createElement("style");
 
-    var head = document.getElementsByTagName('head')[0],
-        style = document.createElement('style');
-
-    style.type = 'text/css';
-    if (style.styleSheet) {
-        style.styleSheet.cssText = css;
-    } else {
-        style.appendChild(document.createTextNode(css));
-    }
-    head.appendChild(style);
+  style.type = "text/css";
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+  head.appendChild(style);
 }
 
 function execute() {
-    chrome.storage.sync.get('colorblindingValue', function (obj) {
-        if (obj.colorblindingValue === null || obj.colorblindingValue === undefined) {
-            obj.colorblindingValue = "normal";
-            chrome.storage.sync.set({'colorblindingValue': obj.colorblindingValue});
-        }
-        changeColors(obj.colorblindingValue);
-    });
+  chrome.storage.sync.get(
+    ["colorblindingValue", "customFilterValues"],
+    function (obj) {
+      if (
+        obj.colorblindingValue === null ||
+        obj.colorblindingValue === undefined
+      ) {
+        obj.colorblindingValue = "normal";
+        chrome.storage.sync.set({ colorblindingValue: obj.colorblindingValue });
+      }
+      changeColors(obj.colorblindingValue, obj.customFilterValues);
+    }
+  );
 }
--- popup.js
+++ popup.js
@@ -2,41 +2,94 @@ var lc1306 = jQuery.noConflict(true);
 
 (function ($) {
 
-
-
     $(function () {
-
-        chrome.storage.sync.get('colorblindingValue', function (obj) {
-
+        chrome.storage.sync.get(["colorblindingValue","customFilterValues"], function (obj) {
             var noValue = obj.colorblindingValue === null || obj.colorblindingValue === undefined;
-            $("input[name=type][value=" + (noValue ? "deactive" : obj.colorblindingValue ) + "]").prop('checked', true);
-
+            $("input[name=type][value=" + (noValue ? "deactive" : obj.colorblindingValue) + "]").prop('checked', true);
             if (obj.colorblindingValue !== 'deactivate' && !noValue) {
+                if (obj.colorblindingValue === 'custom') {
+                    var customValues = obj.customFilterValues;
+
+                    $('input[name="r1"]').val(customValues.r1);
+                    $('input[name="r2"]').val(customValues.r2);
+                    $('input[name="r3"]').val(customValues.r3);
+
+                    $('input[name="g1"]').val(customValues.g1);
+                    $('input[name="g2"]').val(customValues.g2);
+                    $('input[name="g3"]').val(customValues.g3);
+
+                    $('input[name="b1"]').val(customValues.b1);
+                    $('input[name="b2"]').val(customValues.b2);
+                    $('input[name="b3"]').val(customValues.b3);
+
+                    $("#accordion").show();
+                }
+                else {
+                    $("#accordion").hide();
+                }
                 //console.log("internal " + obj.colorblindingValue);
+                //console.log(obj.colorblindingValue);
                 execute();
             }
-
         });
 
         $('input[name="type"]:radio').change(
             function () {
-
                 var newValue = $('input[name=type]:checked', '#cvd_radios').val();
-                chrome.storage.sync.set({'colorblindingValue': newValue}, function () {
+                if (newValue === 'custom') {
+                    $("#accordion").show();
+                }
+                else {
+                    $("#accordion").hide();
+                }
+            });
 
-                    if (newValue !== 'deactivate') {
-                        chrome.tabs.executeScript({file: 'background.js'});
-                    } else {
-                        chrome.tabs.executeScript({file: 'reload.js'});
-                    }
+        $("#btnApply").click(function () {
+            var newValue = $('input[name=type]:checked', '#cvd_radios').val();
+            var customSettings = {};
+            if (newValue === 'custom') {
+                customSettings.r1 = $('input[name="r1"]').val();
+                customSettings.r2 = $('input[name="r2"]').val();
+                customSettings.r3 = $('input[name="r3"]').val();
+                customSettings.r4 = $('input[name="r4"]').val();
 
-                });
+                customSettings.g1 = $('input[name="g1"]').val();
+                customSettings.g2 = $('input[name="g2"]').val();
+                customSettings.g3 = $('input[name="g3"]').val();
+                customSettings.g4 = $('input[name="g4"]').val();
+
+                customSettings.b1 = $('input[name="b1"]').val();
+                customSettings.b2 = $('input[name="b2"]').val();
+                customSettings.b3 = $('input[name="b3"]').val();
+                customSettings.b4 = $('input[name="b4"]').val();
+
+                customSettings.a1 = $('input[name="a1"]').val();
+                customSettings.a2 = $('input[name="a2"]').val();
+                customSettings.a3 = $('input[name="a3"]').val();
+                customSettings.a4 = $('input[name="a4"]').val();
+
+                console.log(customSettings);
             }
-        );
 
-        $("#btnsave").click(function () {
-            chrome.tabs.create({ url: chrome.runtime.getURL("login.html") });
+            chrome.storage.sync.set({ 'colorblindingValue': newValue, 'customFilterValues': customSettings }, function () {
+                if (newValue !== 'deactivate') {
+                    chrome.tabs.executeScript({ file: 'background.js' });
+                } else {
+                    chrome.tabs.executeScript({ file: 'reload.js' });
+                }
+            });
+
+
+            //chrome.tabs.create({ url: chrome.runtime.getURL("login.html") });
         });
+
+        $("#accordion").accordion({
+            heightStyle: "content",
+            header: "h3",
+            collapsible: true,
+            active: false
+        });
+
 
     });
 
