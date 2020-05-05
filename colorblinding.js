9c22b118997ccd86b185d04d52292470be33f453
Mateo Vaquero <mvaquero@albany.edu>
2020-05-05 15:26:11 -0400

    Added Comments

--- colorblinding.js
+++ colorblinding.js
@@ -1,3 +1,10 @@
+
+/* Function to append values to the matrix:
+Open source: https://github.com/Altreus/colourblind 
+Open source: http://lab.leocardz.com/colorblinding
+*/
+
+
 function appendSVG(document, customValues) {
   
   var filterProtanopia =
@@ -17,7 +24,10 @@ function appendSVG(document, customValues) {
   var filterAchromatomaly =
     '<filter id="achromatomaly"> <feColorMatrix type="matrix" values="0.618,0.320,0.062,0,0 0.163,0.775,0.062,0,0 0.163,0.320,0.516,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter>';
 
-  //debugger;
+  /* Filter Function Hamza Bawja and Mateo Vaquero
+     Open Source:http://lab.leocardz.com/colorblinding
+     Contributions: Added new custom filters based on Matrix data, fix values to represent better
+  */;
   var filterCustom =
     '<filter id="custom"> <feColorMatrix type="matrix" values=';
   console.log(customValues);
@@ -31,6 +41,9 @@ function appendSVG(document, customValues) {
       };
     }
 
+    /* Custom Values: Hamza Bawja
+       Novel Contribution
+    */
     var values = '"{0},{1},{2},{3},0 {4},{5},{6},{7},0 {8},{9},{10},{11},0 0,0,0,1,0"'.format(
       customValues.r1,
       customValues.r2,
@@ -52,6 +65,9 @@ function appendSVG(document, customValues) {
   filterCustom += ' in="SourceGraphic" /> </filter>';
   console.log(filterCustom);
 
+  /* SVG storage of values
+  Open source:http://lab.leocardz.com/colorblinding
+  Contributions: Added custom values*/
   var svgA =
     '<svg id="colorBlindSVG" version="1.1" xmlns="http://www.w3.org/2000/svg" baseProfile="full">';
   svgA += filterProtanopia;
@@ -65,23 +81,23 @@ function appendSVG(document, customValues) {
   svgA += filterCustom;
   svgA += "</svg>";
 
-  // var svg = '<svg id="colorBlindSVG" version="1.1" xmlns="http://www.w3.org/2000/svg" baseProfile="full"> <filter id="protanopia"> <feColorMatrix type="matrix" values="0.567, 0.433, 0, 0, 0, 0.558, 0.442, 0, 0, 0, 0, 0.242, 0.758, 0, 0, 0, 0, 0, 1, 0" in="SourceGraphic" /> </filter> <filter id="protanomaly"> <feColorMatrix type="matrix" values="0.817,0.183,0,0,0 0.333,0.667,0,0,0 0,0.125,0.875,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> <filter id="deuteranopia"> <feColorMatrix type="matrix" values="0.625,0.375,0,0,0 0.7,0.3,0,0,0 0,0.3,0.7,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> <filter id="deuteranomaly"> <feColorMatrix type="matrix" values="0.8,0.2,0,0,0 0.258,0.742,0,0,0 0,0.142,0.858,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> <filter id="tritanopia"> <feColorMatrix type="matrix" values="0.95,0.05,0,0,0 0,0.433,0.567,0,0 0,0.475,0.525,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> <filter id="tritanomaly"> <feColorMatrix type="matrix" values="0.967,0.033,0,0,0 0,0.733,0.267,0,0 0,0.183,0.817,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> <filter id="achromatopsia"> <feColorMatrix type="matrix" values="0.299,0.587,0.114,0,0 0.299,0.587,0.114,0,0 0.299,0.587,0.114,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> <filter id="achromatomaly"> <feColorMatrix type="matrix" values="0.618,0.320,0.062,0,0 0.163,0.775,0.062,0,0 0.163,0.320,0.516,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> </svg>';
-
   var blockColorblindContent = document.getElementById(
     "blockColorblindContent"
   );
   if (blockColorblindContent !== undefined && blockColorblindContent !== null) {
     blockColorblindContent.parentNode.removeChild(blockColorblindContent);
   }
-
+  
   var iDiv = document.createElement("div");
   iDiv.id = "blockColorblindContent";
   iDiv.innerHTML = svgA;
-  iDiv.style.display = "none"; //Fix issue "blockColorblindContent" is showing and taking up space on some websites #5
+  iDiv.style.display = "none"; 
   document.getElementsByTagName("body")[0].appendChild(iDiv);
 
-  //console.log(customValues);
 }
+/* Function to apply colors 
+Open source:http://lab.leocardz.com/colorblinding
+Contributuions: Append our custom values*/
 
 function changeColors(type, customValues) {
   console.log("Changing colors...");
@@ -106,12 +122,18 @@ function changeColors(type, customValues) {
   applyingStyle(document, css);
 }
 
+/* Function to revert colors
+Open source: http://lab.leocardz.com/colorblinding
+Contributions: None*/
 function revertColors(document) {
   var css =
     "html { -webkit-filter: none; -moz-filter: none; -o-filter: none; -ms-filter: none; } #blockColorblindContent { display: none; }";
   applyingStyle(document, css);
 }
 
+/* Function to revert colors
+Open source: http://lab.leocardz.com/colorblinding
+Contributions: None*/
 function applyingStyle(document, css) {
   var head = document.getElementsByTagName("head")[0],
     style = document.createElement("style");
@@ -124,6 +146,10 @@ function applyingStyle(document, css) {
   }
   head.appendChild(style);
 }
+
+/* Chrome storage: Israel Ali
+Open source: https://developer.chrome.com/extensions/storage
+Contributions: Added sync method*/
 
 function execute() {
   chrome.storage.sync.get(
